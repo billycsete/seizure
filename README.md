14b9683d5db7919e7bb2ab62ee0e7c632d7d24a7
wcsete <wcsete@gmail.com>
2013-03-03 14:17:54 -0800

    changed pattern of js, updated name to seizure since thats what this is

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-Static
+Seizure
 ======
 
 A canvas grid of constantly changing colors. Trippy.
+
+
+Usage
+------
+
+'var mySeizure = new Seizure('#static');'
+
+
+To Do
+------
+- style inputs and buttons
+- change from setTimeout to request animation frame
+- more controls to play with?
--- scripts/main.js
+++ scripts/main.js
@@ -1,101 +1,122 @@
+(function () {
+	"use strict";
 
-var Static = {
+	var Seizure = function(element) {
+		this.$element = $(element);
+		this.ctx = this.$element[0].getContext('2d');
 
-	init : function(element) {
-		var canvas = $(element);
-		var ctx = canvas[0].getContext('2d');
-		var bgHeight = $(window).height();
-		var bgWidth = $(window).width();
-		var self = this;
+		this.interval = 100;
 
 		// defaults
-		var shapesAccross = 70;
-		var shapesTall = 50;
-		var interval = 100;
+		this.accross = 70;
+		this.tall = 50;
 
-		console.log($('#accross'));
+		this.shapeHeight = $(window).height() / this.tall;
+		this.shapeWidth = $(window).width() / this.accross;
 
-		self.setCanvasSize(canvas);
-
-		$(window).on('resize', function(){
-			self.setCanvasSize(canvas);
-		});
-
-		$('input').change(function() {
-			switch(this.id) {
-				case 'accross':
-					shapesAccross = this.value;
-					$('#accross-val').html(this.value);
-					break;
-				case 'tall':
-					shapesTall = this.value;
-					$('#tall-val').html(this.value);
-					break;
-			}
-		});
-
-		$('#presets button').on('click', function() {
-			switch(this.id) {
-				case 'static-btn':
-					shapesAccross = 180;
-					shapesTall = 160;
-					break;
-				case 'big-squares-btn':
-					shapesAccross = 10;
-					shapesTall = 6;
-					break;
-				case 'small-squares-btn':
-					shapesAccross = 70;
-					shapesTall = 50;
-					break;
-				case 'stripes-btn':
-					$('#tall-val')
-					shapesAccross = 1;
-					shapesTall = 60;
-					break;
-			}
-			$('#accross')[0].value = shapesAccross;
-			$('#tall')[0].value = shapesTall;
-			$('#accross-val').html(shapesAccross);
-			$('#tall-val').html(shapesTall);
-		});
-
-		return setInterval(function() {
-			console.log(shapesTall, shapesAccross);
-			self.drawBackground(ctx, shapesAccross, shapesTall, interval);
-		},interval);
-	},
-
-	drawBackground : function(ctx, accross, tall, interval) {
-		var a;
-		var t;
-		var color;
-		var self = this;
-		var shapeHeight = $(window).height() / tall;
-		var shapeWidth = $(window).width() / accross;
-
-		for (a = 0; a <= accross; a++ ) {
-			for (t = 0; t <= tall; t++ ) {
-				self.drawShape(ctx, shapeWidth * a, shapeHeight * t, shapeWidth, shapeHeight, self.randomColor());
-			}
-		}
-	},
-
-	drawShape : function(ctx, x, y, width, height, color) {
-		ctx.fillStyle = color;
-		ctx.fillRect(x,y,width, height);
-	},
-
-	randomColor : function() {
-		return '#'+Math.floor(Math.random()*16777215).toString(16);
-	},
-
-	setCanvasSize : function(canvas) {
-		canvas.attr('height', $(window).height());
-		canvas.attr('width', $(window).width());
+		this.init();
 	}
-}
+
+	Seizure.prototype = {
+		init : function() {
+			var self = this;
+
+			this.setCanvasSize();
+
+			$(window).on('resize', function(){
+				self.setCanvasSize();
+			});
+
+			this.inputControls();
+			this.addPresets();
+
+			return setInterval(function() {
+				self.drawSeizure();
+			},this.interval);
+		},
+
+		drawSeizure : function() {
+			var a;
+			var t;
+			var color;
+			var self = this;
+			this.shapeHeight = $(window).height() / this.tall;
+			this.shapeWidth = $(window).width() / this.accross;
+
+			for (a = 0; a <= self.accross; a++ ) {
+				for (t = 0; t <= self.tall; t++ ) {
+					self.drawShape(self.shapeWidth * a, self.shapeHeight * t, self.shapeWidth, self.shapeHeight, self.randomColor());
+				}
+			}
+		},
+
+		drawShape : function(x, y, width, height, color) {
+			this.ctx.fillStyle = color;
+			this.ctx.fillRect(x,y,width, height);
+		},
+
+		randomColor : function() {
+			return '#'+Math.floor(Math.random()*16777215).toString(16);
+		},
+
+		setCanvasSize : function() {
+			// update canvas size
+			this.$element.attr('height', $(window).height());
+			this.$element.attr('width', $(window).width());
+		},
+
+		inputControls : function() {
+			var self = this;
+
+			$('input').change(function() {
+				switch(this.id) {
+					case 'accross':
+						self.accross = this.value;
+						$('#accross-val').html(this.value);
+						break;
+					case 'tall':
+						self.tall = this.value;
+						$('#tall-val').html(this.value);
+						break;
+				}
+			});
+		},
+
+		addPresets : function() {
+			var self = this;
+
+			$('#presets button').on('click', function() {
+				switch(this.id) {
+					case 'static-btn':
+						self.accross = 180;
+						self.tall = 160;
+						break;
+					case 'big-squares-btn':
+						self.accross = 10;
+						self.tall = 6;
+						break;
+					case 'small-squares-btn':
+						self.accross = 70;
+						self.tall = 50;
+						break;
+					case 'stripes-btn':
+						$('#tall-val')
+						self.accross = 1;
+						self.tall = 60;
+						break;
+				}
+				$('#accross')[0].value = self.accross;
+				$('#tall')[0].value = self.tall;
+				$('#accross-val').html(self.accross);
+				$('#tall-val').html(self.tall);
+			});
+		}
+	}
+
+	window.Seizure = Seizure;
+})();
+
 
 $(document).ready( function(){
-	Static.init('#static');
+	var mySeizure = new Seizure('#static');
 });
